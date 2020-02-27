d4fb5e9d86a5523393400c5dd5b100ea0d69af93
ajessee <andre.isaac.jessee@gmail.com>
2020-02-27 02:07:59 -0500

    fix issue in seed file to get bin data

--- app/helpers/parser_helper.rb
+++ app/helpers/parser_helper.rb
@@ -2,7 +2,7 @@ module ParserHelper
 
   #Seed database with geolocation of recycling bins in NYC by making call to NYC Open Data API.
   def self.get_data
-    response = HTTParty.get("https://data.cityofnewyork.us/resource/ggvk-gyea.json?$$app_token=" + Rails.application.credentials.dig(:city_of_nyc_api_key))
+    response = HTTParty.get("https://data.cityofnewyork.us/resource/ggvk-gyea.json?$$app_token=#{Rails.application.credentials.dig(:city_of_nyc_api_key)}")
     response.each {|row| Bin.create(row) if row["latitude"] && row["longitude"]}
   end
 
