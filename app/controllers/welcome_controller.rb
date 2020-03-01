9aae3fe06ed95098c303e74d7a8b59e49a4b3124
ajessee <andre.isaac.jessee@gmail.com>
2020-03-01 14:31:01 -0500

    add new controller, update all views, update routes

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class WelcomeController < ApplicationController
+  def hello; end
+
+  def fetch_closest_bin
+    @user_coordinates = Geokit::LatLng.new(bin_params[:lat].to_f,bin_params[:lng].to_f)
+    @bin = Bin.closest(origin: @user_coordinates).first
+    respond_to do |format|
+      format.js {
+        render json: {
+          binLat: @bin.latitude,
+          binLng: @bin.longitude
+        }
+      }
+    end
+  end
+
+  def fetch_coords_and_bin
+    address = bin_params['street'] + ', ' + bin_params['city'] + ', ' + bin_params['zip']
+    address_data = Geokit::Geocoders::GoogleGeocoder.geocode address
+    @address_coordinates = Geokit::LatLng.new(address_data.lat, address_data.lng)
+    @closest_bin = Bin.closest(origin: @address_coordinates).first
+    respond_to do |format|
+      format.js {
+        render json: {
+          addressLat: @address_coordinates.lat,
+          addressLng: @address_coordinates.lng,
+          binLat: @closest_bin.latitude,
+          binLng: @closest_bin.longitude
+        }
+      }
+    end
+  end
+
+  private
+
+  def bin_params
+    params.require(:welcome).permit(:lat, :lng, :street, :city, :zip)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root 'welcome#hello'
+  get 'welcome/hello'
+  post 'welcome/fetch_closest_bin', to: 'welcome#fetch_closest_bin'
+  post 'welcome/fetch_coords_and_bin', to: 'welcome#fetch_coords_and_bin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  root 'bins#index'
-
-  resources :bins, only: [:index, :show]
-    post 'bins/getlatlng', :to => 'bins#getlatlng'
-    post 'bins/walking_directions', :to => 'bins#walking_directions'
-    post 'bins/street_view', :to => 'bins#street_view'
-    post 'bins/convert_to_latlng', :to => 'bins#convert_to_latlng'
 end
