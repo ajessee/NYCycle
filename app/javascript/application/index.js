de30fc6b8e90807ea4892507648377dc55a0bc62
Andre Jessee <andre.isaac.jessee@gmail.com>
2020-03-02 17:31:23 -0500

    make mobile height dynamic for better viewing experience across devices, update address input form dimensions, add port 5433 to test database

--- app/javascript/application/index.js
+++ app/javascript/application/index.js
@@ -1,10 +1,18 @@
-// Setup main JS object
-window.NYCycle = {};
-
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
 
 const application = require.context('.', true, /.js$/)
 application.keys().forEach(application)
 
+// Setup main JS object
+window.NYCycle = {
+    viewHeight: window.innerHeight * 0.01
+};
 
+document.documentElement.style.setProperty('--vh', `${window.NYCycle.viewHeight}px`);
+
+// recalculate the --vh viewHeight CSS variable on window resize
+window.addEventListener('resize', () => {
+    window.NYCycle.viewHeight = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${window.NYCycle.viewHeight}px`);
+});
